6eaa34b0d3f46d96f8d69a23291b30cd7f96d684
Ivan H. S <ivan.slaughter@gmail.com>
2021-12-13 04:28:34 +0700

    Update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
+import isMobile from './mobile.js';
 import { Map, View, Overlay } from 'ol';
 import { Draw, Modify, Select, Snap } from 'ol/interaction';
 import MousePosition from 'ol/control/MousePosition';
@@ -14,11 +15,11 @@ import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
 import OSM from 'ol/source/OSM';
 import { createStringXY } from 'ol/coordinate';
 import { ScaleLine, OverviewMap, ZoomToExtent, defaults as defaultControls } from 'ol/control';
-import { fromLonLat, useGeographic, Projection } from 'ol/proj';
+import { fromLonLat, useGeographic, Projection, transformExtent } from 'ol/proj';
 import { Modal, Offcanvas } from 'bootstrap';
 import numeral from 'numeral';
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, addDoc, getDoc, getDocs, setDoc, query, where, orderBy, Timestamp } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc, addDoc, getDoc, getDocs, setDoc, query, where, orderBy, Timestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYng2dI6yeBkrJfm7ygBn61KHyHw0tf1M",
@@ -78,18 +79,7 @@ numeral.register('locale', 'id', {
   }
 });
 
-// switch between locales
 numeral.locale('id');
-
-//const isMobile = navigator.userAgentData.mobile;
-let isMobile = false; //initiate as false
-// device detection
-if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-  || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
-  isMobile = true;
-}
-
-
 
 let editMode = false;
 let modalEdit = new Modal(document.getElementById('modalEdit'));
@@ -112,8 +102,14 @@ const subStats = document.getElementById('sub-stats');
 
 const mapCenter = fromLonLat([98.1969, 4.2667]);
 
+function transform(extent) {
+  return transformExtent(extent, 'EPSG:4326', 'EPSG:3857');
+}
+
+const mapExtent = transform([98.17655462693399, 4.233276920014973, 98.2303961838639, 4.298548400759917]);
+
 let mapPadding = [50, 198, 216, 247.68];
-if (isMobile) {
+if (isMobile()) {
   mapPadding = [90, 30, 90, 90];
 }
 
@@ -128,7 +124,7 @@ const overviewMapControl = new OverviewMap({
 
 const mousePositionControl = new MousePosition({
   coordinateFormat: createStringXY(4),
-  projection: 'EPSG:4326',
+  projection: 'EPSG:32647',
   // comment the following two lines to have the mouse position
   // be placed within the map.
   className: 'custom-mouse-position',
@@ -198,6 +194,8 @@ const map = new Map({
   layers: layers,
   view: view,
 });
+
+map.getView().fit(mapExtent);
 
 map.on('singleclick', function (evt) {
   if (!editMode) {
@@ -446,9 +444,10 @@ function setMapInfos(layerJson) {
           <div class="d-flex flex-column mt-2">`;
           arrCol.forEach(element_ => {
             if (element_ != 'Id') {
+              let el_label = (element_ == 'Jml_Sawit') ? 'Jumlah Sawit' : element_;
               content += `
               <div id="${element_}" class="stats-box">
-              <div class="stats-label">${element_}</div>
+              <div class="stats-label">${el_label}</div>
               <div id="stats-density" class="stats-value">${isNaN(element.properties[element_]) ? element.properties[element_] : numeral(element.properties[element_]).format(',0')}</div>
             </div>`;
             }
@@ -467,12 +466,22 @@ function setMapInfos(layerJson) {
 
   if (nullLayer) {
     //popup
-    $('#main-stats').slideUp();
+    //$('#main-stats').slideUp();
+    let mainStats = document.getElementById('main-stats');
+    let subStats = document.getElementById('sub-stats');
+    
+    mainStats.classList.remove('show');
     subStats.innerHTML = content;
-    $('#sub-stats').on('click', '#back-stats', function () {
-      $('#main-stats').show();
+    subStats.classList.add('show');
+
+    document.getElementById('back-stats').addEventListener('click', event => {
+      mainStats.classList.add('show');
+      subStats.classList.remove('show');
       reset.mapView();
+      map.getView().fit(mapExtent);
     });
+
+
 
     if (selectedFilter === 'blok') {
       firestoreModule.getPupuks(layerId).then(function (data) {
@@ -677,7 +686,7 @@ function saveFeature() {
 }
 
 window.addEventListener('DOMContentLoaded', event => {
-  if (isMobile) {
+  if (isMobile()) {
     document.querySelector("body").classList.add('mobile');
     document.getElementById('filters').classList.remove('show');
     document.getElementById('select-filters').classList.remove('show');
@@ -688,7 +697,7 @@ window.onresize = function () { location.reload(); };
 
 const menuToggle = document.getElementById('menu-toggle');
 menuToggle.addEventListener('click', event => {
-  if (!isMobile) {
+  if (!isMobile()) {
     document.querySelector('body').classList.add('mobile');
   }
 })
